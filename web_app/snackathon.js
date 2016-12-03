697731b76f79f3b67fd3d2871404af3f059dde5c
AW <alex.m.weiss2@gmail.com>
2016-12-03 13:12:06 -0500

    add js file

---
+++ web_app/snackathon.js
@@ -0,0 +1,21 @@
+menuitems = {}
+function getitemlist()
+{
+	
+}
+function additem(loc)
+{
+	
+}
+function loaditem(itemid)
+{
+	
+}
+function edititem(itemid)
+{
+	
+}
+function deleteitem(itemid)
+{
+	
+}
